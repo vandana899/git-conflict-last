e77e4b813e41463ecfebb55940a0108807b363c9
vandana899 <112388492+vandana899@users.noreply.github.com>
2023-12-06 07:57:41 +0530

    Add files via upload

---
+++ mergeconflictt/src/test/java/userA/actionsuser.java
@@ -0,0 +1,15 @@
+package userA;
+
+import org.testng.annotations.Test;
+
+public class actionsuser {
+@Test
+public void bb() {
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
+	System.out.println("1");
+	
+}
+}
